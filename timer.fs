24dcd5c4362873e50d921db29111be51ebcd6283
jacereda <jacereda@localhost>
2007-01-11 01:45:31 +0000

    Made it reentrant

--- timer.fs
+++ timer.fs
@@ -2,8 +2,6 @@ require ffi.fs
 library librt rt
 librt clock_gettime int ptr (int) clock_gettime
 
-create nsres 0 , 0 , \ XXX make it reentrant
 \g Returns real-time clock in nanoseconds
 : nstime ( -- d )
-   0 nsres clock_gettime abort" Unable to get time" nsres 2@ ;
-
+   0 0 0 sp@ cell+ clock_gettime abort" Unable to get time" ;
