158a94d16fabea2db0f58db5c6b9b51a7c2a7afb
jacereda <jacereda@localhost>
2007-01-21 18:45:47 +0000

    Library handles are restored in coldchain

--- ffi.fs
+++ ffi.fs
@@ -1,5 +1,6 @@
 : lastbody ( -- addr )
    lastname name>xt xt>body ;
+
 : fn ( libhandle "name" -- )
    lastbody 9 cells + \ args
    here cell- @ \ ret
@@ -7,6 +8,7 @@
    lastbody cell+ \ cif  
    ffprep abort" Unable to prepare function call" 
    0 parse rot dlsym dup 0= abort" Unable to lookup symbol" lastbody ! ;
+
 : int ( -- ) ffint , ;
 : sf ( -- ) fffloat , ;
 : df sf ; \ XXX
@@ -15,21 +17,33 @@
 : (float) ( -- ) fffloat , fn ;
 : (ptr) ( -- ) ffptr , fn ;
 : (void) ( -- ) ffvoid , fn ;
+
 : newfun ( lib -- )
    create 0 , 8 cells allot \ func|cif, will be filled by "ret"
    does> dup @ swap cell+ ffcall ;
+
 : trylib
    s" lib" pad place 2swap pad append pad append  pad count dlopen ;
-: library ( "forthname" "libname" ) 
-   create 0 parse 
+
+variable libs libs off
+
+: openlib ( addr len -- handle )
    0 >r
    2dup s" " trylib r> or >r
    2dup s" .dylib" trylib r> or >r
    2dup s" .so" trylib r> or >r
    2dup s" .so.6" trylib r> or >r   \ Special case for libc
    2drop r>
-   dup 0= abort" Unable to open library" , 
+   dup 0= abort" Unable to open library" ;
+
+: library ( "forthname" "libname" ) 
+   create here 0 , libs linked 0 parse 2dup s,
+   openlib swap !
    does> @ newfun ;
+
+:noname
+   libs begin @ dup while dup cell+ count openlib over cell- ! repeat 
+   deferred coldchain ; is coldchain
 
 0 [if]
 library libc c
