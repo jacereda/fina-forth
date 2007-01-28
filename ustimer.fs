ccdd02a1031c2a98dd3d41d28c8c7c109997ee1d
jacereda <jacereda@localhost>
2007-01-28 19:04:56 +0000

    Fixed timer

--- ustimer.fs
+++ ustimer.fs
@@ -1,4 +1,7 @@
 \ Don't use this, include timer.fs
 libc gettimeofday ptr int (int) gettimeofday
-: sus ( -- seconds microseconds)
-   0 0 sp@ 0 gettimeofday -21 ?throw swap ;
+: sns ( -- seconds nanoseconds)
+   0 0 sp@ 0 gettimeofday -21 ?throw swap 1000 * ;
+sns 2drop
+: nstime ( -- d)
+   sns >r 1000000000 m* r> m+ ;
