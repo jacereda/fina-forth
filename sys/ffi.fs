95b3d003889566b5aacd84bd00b5382cd4d8b5b7
jacereda <jacereda@localhost>
2007-01-30 21:38:10 +0000

    0-terminated strings

--- sys/ffi.fs
+++ sys/ffi.fs
@@ -49,6 +49,13 @@ variable libs libs off
    openlib swap !
    does> @ newfun ;
 
+create 0buffer 256 allot
+: 0term 0buffer place 0 0buffer count + c! 0buffer 1+ ;
+: 0count 0 over begin dup c@ while 1 under+ 1+ repeat drop ;
+: 0s, 1+ s, 0 here 1- c! ;
+: 0sliteral postpone dos" 0s, align postpone drop ;
+: 0s"  [char] " parse 0sliteral ; immediate compile-only
+
 :noname
    libs begin @ dup while dup cell+ count openlib over cell- ! repeat drop
    deferred coldchain ; is coldchain
