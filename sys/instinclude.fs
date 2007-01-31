220b4762aae1aea624ed16f7f29d0d9e17698ee3
jacereda <jacereda@localhost>
2007-01-31 01:34:44 +0000

    Altering LD_LIBRARY_PATH at runtime doesn't affect the current process

--- sys/instinclude.fs
+++ sys/instinclude.fs
@@ -1,6 +1,4 @@
 libc 0dirname ptr (ptr) dirname
-libc 0getenv ptr (ptr) getenv
-libc 0setenv ptr ptr int (int) setenv
 
 : dirname ( addr len -- addr' len' )
    0term 0dirname dup 0= -37 ?throw 0count ;
@@ -25,15 +23,3 @@ libc 0setenv ptr ptr int (int) setenv
 
 :noname
    2dup file-exists? 0= if >share then deferred inchook0 ; is inchook0
-
-: 0+path ( val var -- )
-   >r  r@ 0getenv ?dup if 0count else s" " then pad place
-   s" :" pad append
-   0count pad append
-   r> pad count 0term 1 0setenv ;
-
-\ Add our lib path to lib search path
-:noname
-\   s" lib/fina/" >inst 0term 0s" LD_LIBRARY_PATH" 0+path
-\   s" lib/fina/" >inst 0term 0s" DYLD_LIBRARY_PATH" 0+path
-   deferred coldchain ; is coldchain
