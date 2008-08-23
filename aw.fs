6c7d6663182b84a3e40985050f9abe435a38eb4e
jacereda <jacereda@localhost>
2008-08-23 16:35:15 +0000

    Adapted aw.fs to new libaw

--- aw.fs
+++ aw.fs
@@ -1,16 +1,11 @@
 \ require ffi.fs
 library aw aw
-
 aw awInit (int) awInit
 aw awEnd (void) awEnd
-aw awOpen (ptr) awOpen
+aw awOpen ptr int int int int (ptr) awOpen
 aw awClose ptr (void) awClose
 aw awSwapBuffers ptr (void) awSwapBuffers
-aw awMakeCurrent ptr (void) awMakeCurrent
-aw awShow ptr (void) awShow
-aw awHide ptr (void) awHide
-aw awSetTitle ptr ptr (void) awSetTitle
-aw awMove ptr int int (void) awMove
-aw awResize ptr int int (void) awResize
+aw awPushCurrent ptr (void) awPushCurrent
+aw awPopCurrent ptr (void) awPopCurrent
 aw awNextEvent ptr (ptr) awNextEvent
 
