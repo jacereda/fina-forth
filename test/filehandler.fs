55e48f72ec8ed21ce1a31f22e1e5c0792e0d1f3f
jacereda <jacereda@localhost>
2008-08-31 01:32:48 +0000

    Adapted handler tests

--- test/filehandler.fs
+++ test/filehandler.fs
@@ -1,6 +1,11 @@
-.( opening) cr
-char " parse handler.fs" r/o open .( opened) cr throw constant h
-.( reading) cr
-pad 256 h read throw drop
+require filehandler.fs
+TESTING FILEHANDLER
+{ char " parse handler.fs" r/o file rawopen throw constant rh -> }
+{ pad 256 rh file read throw -> 256 }
 pad 256 dump
-h close
+{ rh file close throw -> }
+{ char " parse handler.fs" r/o open throw 2constant h -> }
+{ pad 256 h read throw -> 256 }
+pad 256 dump
+{ h close throw -> }
+
