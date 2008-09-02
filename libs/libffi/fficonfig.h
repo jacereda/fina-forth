de006aebaa2a423782db6aabfceb2bf222926078
jacereda <jacereda@localhost>
2008-09-02 21:06:34 +0000

    We don't use the raw api

--- libs/libffi/fficonfig.h
+++ libs/libffi/fficonfig.h
@@ -16,7 +16,7 @@
 /* #undef FFI_DEBUG */
 
 /* Define this is you do not want support for the raw API. */
-/* #undef FFI_NO_RAW_API */
+#define FFI_NO_RAW_API 1
 
 /* Define this is you do not want support for aggregate types. */
 /* #undef FFI_NO_STRUCTS */
