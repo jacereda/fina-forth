2d660d10ffe3b666c47c23d5430cf7655d395002
jacereda <jacereda@localhost>
2008-11-28 17:56:43 +0000

    Added some more functions

--- gl.fs
+++ gl.fs
@@ -1,6 +1,13 @@
-
 library GL GL
 hex
+
+8892 constant GL_ARRAY_BUFFER
+8893 constant GL_ELEMENT_ARRAY_BUFFER
+88E4 constant GL_STATIC_DRAW
+88B9 constant GL_WRITE_ONLY
+
+
+cconstant GL_TRUE
 cconstant GL_COLOR_BUFFER_BIT
 cconstant GL_DEPTH_BUFFER_BIT
 cconstant GL_TRIANGLES
@@ -12,7 +19,10 @@ cconstant GL_DEPTH_TEST
 cconstant GL_LIGHTING
 cconstant GL_LIGHT0
 cconstant GL_FLOAT
+
 decimal
+
+GL glGetError (int) glGetError
 
 GL glShadeModel int (void) glShadeModel
 GL glMatrixMode int (void) glMatrixMode
@@ -32,3 +42,11 @@ GL glVertex2f sf sf (void) glVertex2f
 GL glTranslatef sf sf sf (void) glTranslatef
 
 GL glOrtho df df df df df df (void) glOrtho
+
+GL glUseProgram int (void) glUseProgram
+
+GL glGenBuffers int ptr (void) glGenBuffers
+GL glMapBuffer int int (ptr) glMapBuffer
+GL glUnmapBuffer int (int) glUnmapBuffer
+GL glBindBuffer int int (void) glBindBuffer
+GL glBufferData int int ptr int (void) glBufferData
