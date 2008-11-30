af257d085b6a665c0071435316bce2bccd98c2d0
jacereda <jacereda@localhost>
2008-11-30 11:18:13 +0000

    LATER and NOW

---
+++ later.fs
@@ -0,0 +1,9 @@
+: later r> r> 2>r ;
+: now 2r> r> 2>r >r ;
+
+1 [if]
+: t1 later ." attrib1 " ;
+: t2 later ." attrib2 " ;
+:noname t1 t2 ." attribs: " ; execute cr
+:noname t1 t2 ." attribs: " now ." done " ; execute cr
+[then]
