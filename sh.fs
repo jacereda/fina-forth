8e2fdeac1160c5b0f1cca196ff231e7c5f4a90ea
jacereda <jacereda@localhost>
2008-12-01 21:08:58 +0000

    Hide system

--- sh.fs
+++ sh.fs
@@ -1,5 +1,8 @@
+expose-module private
 libc system ptr (int) system
 : (sh) 0term system throw ;
 : sh" 
    [char] " parse postpone sliteral postpone (sh) ; immediate compile-only
 : sh 0 parse (sh) ;
+export sh sh" (sh)
+end-module
