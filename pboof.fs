f81ad84a98f05b331517697d0eefe85deae3379e
jacereda <jacereda@localhost>
2008-12-04 22:41:36 +0000

    object arena

--- pboof.fs
+++ pboof.fs
@@ -8,18 +8,44 @@ ostack /ostack + value otos
 : o@ otos @ ;
 : o> o@ odrop ;
 
+1024 16 * constant /oarena
+create o0 /oarena allot
+here constant otop
+o0 value ohere
+
+0 value sdict0
+0 value sdicttop
+0 value shere
+
+: >oarena 
+   dict0 to sdict0  dicttop to sdicttop  here to shere
+   o0 to dict0  otop to dicttop  ohere to here  ;
+: oarena>
+   here to ohere 
+   sdict0 to dict0  sdicttop to dicttop  shere to here ;
+
+>oarena 0 , oarena>
+
 : { dup +order state @ if postpone literal postpone >o else >o then ; immediate
 : } previous state @ if postpone odrop else odrop then ; immediate
 : odefs get-current o> swap >o >o o@ set-current ;
-create object immediate wordlist 3 cells ,
+
+>oarena wordlist 3 cells , oarena> constant object immediate
+
 object { odefs
 : self o@ ;
+.s
 : /object self cell+ cell+ ;
 : extend odefs ;
 : extended o> o> swap >o set-current ;
-: member ( size "name" -- ) create /object @ , /object +! does> @ self + ;
-: cloned self /object self @ wordlist ! here over @ dup allot move ;
-: clone ( "name" -- ) create immediate cloned ;
+: member ( size "name" -- ) 
+   >oarena dup allot oarena> 
+   create /object @ , /object +! does> @ self + ;
+: cloned ( -- o )
+   >oarena here
+   self /object self @ wordlist ! here over @ dup allot move 
+   oarena> ;
+: clone ( "name" -- ) cloned constant immediate ;
 : dump  self /object @ dump ;
 : .slots self [ expose-module private ] wordsin [ end-module ] ;
 : late ( "method" -- ? ) 
