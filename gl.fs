0baffd690c7ea285ebc4ff55914faee93d67e2bb
jacereda <jacereda@localhost>
2008-12-04 21:36:07 +0000

    Some more funcs/constants

--- gl.fs
+++ gl.fs
@@ -19,6 +19,9 @@ cconstant GL_DEPTH_TEST
 cconstant GL_LIGHTING
 cconstant GL_LIGHT0
 cconstant GL_FLOAT
+cconstant GL_INT
+cconstant GL_SHORT
+cconstant GL_VERTEX_ARRAY
 
 decimal
 
@@ -28,9 +31,12 @@ GL glShadeModel int (void) glShadeModel
 GL glMatrixMode int (void) glMatrixMode
 GL glClearColor sf sf sf sf (void) glClearColor
 
+GL glColor4f sf sf sf sf (void) glColor4f
 GL glColor4uiv ptr (void) glColor4uiv
+GL glColor4fv ptr (void) glColor4fv
 GL glVertexPointer int int int ptr (void) glVertexPointer
 GL glEnable int (void) glEnable
+GL glEnableClientState int (void) glEnableClientState
 GL glViewport int int int int  (void) glViewport
 GL glClear int  (void) glClear
 GL glLoadIdentity (void) glLoadIdentity
@@ -40,6 +46,7 @@ GL glFlush (void) glFlush
 GL glVertex3iv ptr (void) glVertex3iv
 GL glVertex2f sf sf (void) glVertex2f
 GL glTranslatef sf sf sf (void) glTranslatef
+GL glRotatef sf sf sf sf (void) glRotatef
 
 GL glOrtho df df df df df df (void) glOrtho
 
@@ -50,3 +57,5 @@ GL glMapBuffer int int (ptr) glMapBuffer
 GL glUnmapBuffer int (int) glUnmapBuffer
 GL glBindBuffer int int (void) glBindBuffer
 GL glBufferData int int ptr int (void) glBufferData
+
+GL glDrawArrays int int int (void) glDrawArrays
