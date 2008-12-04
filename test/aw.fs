591ca6bbf2dc2735706bfc22e99de345ae72ea15
jacereda <jacereda@localhost>
2008-12-04 21:36:39 +0000

    0term is already defined

--- test/aw.fs
+++ test/aw.fs
@@ -1,7 +1,5 @@
 require aw.fs
 require cce.fs
-create buffer 256 allot
-: 0term buffer place 0 buffer count + c! buffer 1+ ;
 
 :noname awInit 0= abort" Unable to initialize" ; execute
 char " parse AW test" 0term 
