616861fb215148edc8efdde34e9cd05828b49421
jacereda <jacereda@localhost>
2008-12-15 20:43:28 +0000

    Added crude AS implementation to pose as another object Most private words live now in the private module

--- pboof/rect.fs
+++ pboof/rect.fs
@@ -2,6 +2,8 @@ object clone rect
 rect extend
 vector2 instance pos
 vector2 instance dim
-: pack ( x y w h -- )  dim pack  pos pack ;
+: pospack pos pack ;
+: dimpack dim pack ;
+: pack ( x y w h -- )  dimpack  pospack ;
 : unpack ( -- x y w h )  pos unpack  dim unpack ;
 extended
