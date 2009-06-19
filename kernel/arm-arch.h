794f7f3a0e61bea52a2ccbcc2443ad7cba892094
jacereda <jacereda@localhost>
2009-06-19 16:39:43 +0000

    Adapted to new interface

--- kernel/arm-arch.h
+++ kernel/arm-arch.h
@@ -1,17 +1,17 @@
 #define PRIMSATTR
 
-#define SAVESP
-#define RESTORESP
-register CELL * rsp asm("%r7");
-register CELL * fpc asm("%r6");
-register CELL * dsp asm("%r5");
-register CELL   tos asm("%r4");
+#define CALLSAVE
+#define CALLREST
+#define RSPREG //asm("%r7");
+#define FPCREG //asm("%r6");
+#define DSPREG //asm("%r5");
+#define TOSREG //asm("%r4");
 
 
 static inline CELL * getlnk()
 {
 	register volatile CELL * lnk asm("%lr");
-	return lnk;
+	return (CELL*)lnk;
 }
 
 
@@ -31,7 +31,7 @@ static inline CELL arch_calledby(CELL xt)
         t &= 0x00ffffff;
         t <<= 8;
         t >>= 6;
-        t += tos;
+        t += xt;
         t += 2 * sizeof(CELL);
         return t;
 }
