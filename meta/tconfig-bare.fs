19754de8cd34eeddcc9838dd1a0515569ff90ca4
jacereda <jacereda@localhost>
2010-06-24 23:55:00 +0000

    Added missing file

---
+++ meta/tconfig-bare.fs
@@ -0,0 +1,15 @@
+\ All these values are expressed in bytes
+
+256        constant /pad     \ Pad size
+256        constant /tib     \ Terminal Input Buffer size
+256        constant /hld     \ HOLD buffer size
+256        constant /user    \ Space reserved for user variables
+1024       constant /ds      \ Data stack size
+1024       constant /rs      \ Return stack size
+256 1024 * constant /tdict   \ Dictionary size
+
+\ These should be tweakable, but the sad truth is they aren't
+-1 constant has-ffi
+0 constant has-files
+0 constant has-allocate
+-1 constant more-prims
