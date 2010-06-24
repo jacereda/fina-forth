663a3d3bab6bcc0689a684ca04f9e73f3f036ac0
jacereda <jacereda@localhost>
2010-06-24 21:34:10 +0000

    FFI configuration flag

--- meta/tconfig.fs
+++ meta/tconfig.fs
@@ -9,6 +9,7 @@
 256 1024 * constant /tdict   \ Dictionary size
 
 \ These should be tweakable, but the sad truth is they aren't
+-1 constant has-ffi
 -1 constant has-files
 -1 constant has-allocate
 -1 constant more-prims
