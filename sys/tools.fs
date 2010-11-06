f22396fccafa355b5188a0a5ad8fa7b80684b11b
jacereda <jacereda@localhost>
2010-11-06 00:27:30 +0000

    Some SEE improvements

--- sys/tools.fs
+++ sys/tools.fs
@@ -59,8 +59,28 @@ expose-module private
 : dump  ( addr u -- )
    16 based (dump) ;
 
+\g returns true if the val could be a PC address
+: pc? ( val -- flag )
+   dup aligned over = swap dict? and ;
+
+\g returs true if val could be an xt
+: xt? ( val -- flag )
+   dup pc? if ?dodefine nip 0<> else primxt? then ;
+
+: primheader? ( val -- flag )
+   dup pc? if 
+      dup @ 3735928559 = if
+         cell+ @ primxt? exit
+      then
+   then drop false ;
+
 : nextxt ( xt1 -- xt2 true | here false )
-   begin dup here = if false exit then cell+ dup ?dodefine nip until true ;
+   begin 
+      cell+
+      dup here = if false exit then 
+      dup primheader? if cell+ @ true exit then
+      dup xt? if true exit then 
+   again ;
 
 : /xt ( xt -- a-addr )
    dup primxt? if  
@@ -73,7 +93,9 @@ expose-module private
    ?dodefine nip @r+ = ;
 
 : doersee ( xt -- )
-   dup type? dolist if ." : " then
+   dup type? dolist if 
+      ." :" dup xt>name if space then
+   then
    dup type? dovar if ." variable " then
    dup type? doconst if ." constant " then
    dup type? douser if ." user " then
@@ -81,20 +103,22 @@ expose-module private
    dup primxt? if ." primitive " then
    drop ;
 
-\g returns true if the val could be a PC address
-: pc? ( val -- flag )
-   dup aligned over = swap dict? and ;
-
-\g returs true if val could be an xt
-: xt? ( val -- flag )
-   dup pc? if ?dodefine nip 0<> else primxt? then ;
-
 : safext>name ( xt -- name|0 )
-   dup xt? if xt>name else 0 (d.) pad place pad then ;
+   dup xt? if 
+      xt>name dup 0= if drop s" noname" pad place pad then
+   else 
+      s" [ " pad place 
+      0 (d.) pad append
+      s"  , ] " pad append
+      pad 
+   then ;
 
-: cellsee ( a-addr1 -- a-addr2 )
-   dup @ safext>name .name 
-   cell+ ;
+: cellsee ( /xt a-addr1 -- /xt' a-addr2 )
+   2dup cell+ = over @ ['] exit = and if 
+      ." ;" 
+   else
+      dup @ safext>name .name 
+   then cell+ ;
 
 defer codesee  
 :noname ( xt /xt -- )  over - dump ; is codesee
@@ -102,22 +126,22 @@ defer codesee
 \g Examine code for xt
 : (xtsee) ( xt -- )
    dup doersee 
-   dup xt>name .name
-   dup primxt? if dup else dup ?dodefine nip then xt>name .name
+   dup safext>name .name
    dup /xt 
    over primxt? if codesee exit then 
    swap ?dodefine drop 
    begin  2dup >  while  cellsee  repeat 2drop ;
 
 : xtsee ( xt -- )
-   dup (xtsee) xt>name dup 
-   fimmed 0 > if ." immediate "  then   
-   /fcompo unless ." compile-only" then
-   cr ;
+   dup (xtsee) xt>name ?dup if 
+      dup 
+      fimmed 0 > if ." immediate "  then   
+      /fcompo unless ." compile-only" then
+   then cr ;
 
 \g @see anstools
 : see  ( "<spaces>name" -- )
    ' xtsee ;
 
-export see xtsee dump
+export see xtsee dump xt?
 end-module
