731f4c22c850474e447c1a2972f0e7a7c926aa66
jacereda <jacereda@localhost>
2010-12-04 22:29:49 +0000

    Fix nesting of interpreted conditionals

--- sys/toolsext.fs
+++ sys/toolsext.fs
@@ -1,11 +1,7 @@
 expose-module private
 
-\g Syntactic sugar used in combination with FOREACHPARSED
-: apply ( "word" --  xt )
-   postpone ['] ;  immediate compile-only
-
 \g apply xt to each word parsed. The xt stack picture must be
-\g ( i*x a u -- i*x 0 | j*x -1). If tos=0 the parsing process ends.
+\g ( i*x a u -- i*x 0 | j*x -1). If tos is true the parsing process ends.
 : foreachparsed ( xt -- )
    >r begin
       begin
@@ -17,14 +13,14 @@ expose-module private
 
 \ helper word for [else]
 : []handle
-   s" [if]"   2over icompare unless 2drop 1+ -1 exit then
-   s" [else]" 2over icompare unless 2drop 1- -1 exit then
-   s" [then]" 2over icompare unless 2drop 1- -1 exit then 
-   2drop 0 ; 
+   s" [if]"   2over icompare unless 2drop 1+ false exit then
+   s" [else]" 2over icompare unless 2drop dup 1 = + dup 0= exit then
+   s" [then]" 2over icompare unless 2drop 1- dup 0= exit then 
+   2drop false ; 
 
 \g @see anstools
 : [else] 
-   1 begin apply []handle foreachparsed dup 0= until drop ; immediate
+   1 ['] []handle foreachparsed drop ; immediate
 
 \g @see anstools
 : [if] 
