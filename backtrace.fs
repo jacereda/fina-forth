f34f514e8dee0dcb20afa4beaa14b848abc896de
Jorge Acereda <jacereda@gmail.com>
2010-12-06 22:55:16 +0100

    Fixed crash when printing backtraces, added ~~ word

--- backtrace.fs
+++ backtrace.fs
@@ -1,14 +1,22 @@
-
+: nearestname ( xt -- c-addr )
+  begin dup xt>name 0= while cell- repeat xt>name ;   
+: numrepr ( u -- c-addr )
+  s" h# " pad place  16 based repr pad append  pad ;
 : probablename ( xt -- name )
-    dup dict? if 
-        begin dup xt>name 0= while cell- repeat xt>name 
-    else
-	s" h# " pad place  16 based repr pad append  pad
-    then ;
+    dup aligned over =  over dict? and if nearestname else numrepr then ;
 : rstack ( -- bottom top )
     rp0 @ rp@ ;
 : .backtrace ( bottom top -- )
     swap cell+ swap
-    ." backtrace: " do i @ probablename .name 1 cells +loop ;
+    ." backtrace: " 2dup - 1 cells / .depth
+    do i @ probablename .name 1 cells +loop ;
 : .stacks ( -- )
    .s rstack .backtrace cr ;
+: (~~) type [char] : emit . .stacks cr ;
+: ~~ 
+   state @ if
+      sourceline# postpone literal sourcefilename postpone sliteral
+      postpone (~~)
+   else 
+      sourceline# sourcefilename (~~)
+   then ; immediate
