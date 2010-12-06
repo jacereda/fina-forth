b472cad47b0380cfe4fd9603ad4842c5804c99de
Jorge Acereda <jacereda@gmail.com>
2010-12-06 23:24:40 +0100

    Doubled dictionary size

--- meta/tconfig.fs
+++ meta/tconfig.fs
@@ -2,10 +2,10 @@
 : tcells ( u -- u')
    tcellbits 8 / * ;
 
-256        constant /pad     \ Pad size
-256        constant /tib     \ Terminal Input Buffer size
-256        constant /hld     \ HOLD buffer size
-64         constant /user    \ cells reserved for user variables
-256        constant /ds      \ Data stack size in cells
-256        constant /rs      \ Return stack size in cells
-256 1024 * constant /tdict   \ Dictionary size in bytes
+256         constant /pad     \ Pad size
+256         constant /tib     \ Terminal Input Buffer size
+256         constant /hld     \ HOLD buffer size
+64          constant /user    \ cells reserved for user variables
+256         constant /ds      \ Data stack size in cells
+256         constant /rs      \ Return stack size in cells
+512 1024 *  constant /tdict   \ Dictionary size in bytes
