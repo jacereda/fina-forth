089e875dcd16f813e05336ba5c660dd39428cf2e
Jorge Acereda <jacereda@gmail.com>
2010-12-08 12:39:44 +0100

    Test EOF behaviour

--- test/file.fs
+++ test/file.fs
@@ -13,7 +13,9 @@ decimal
 { pad 80 file read-file -> 80 0 }
 { fsize 0 -40 -1 d+ file reposition-file -> 0 }
 { file file-position -> fsize 0 -40 -1 d+ 0 }
-{ pad 41 file read-file -> 40 -39 }
+{ pad 41 file read-file -> 40 0 }
+{ pad 41 file read-file -> 0 -39 }
+{ pad 41 file read-file -> 0 -39 }
 { pad 80 file read-line -> 0 0 0 }
 { file file-position -> fsize 0 0 }
 { 0 file-position -> -1 -1 -37 }
