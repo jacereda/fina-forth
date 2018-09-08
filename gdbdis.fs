45896d492942b877d8b8bf904cc06d3cbbc08444
Jorge Acereda <jacereda@gmail.com>
2018-09-08 10:19:35 +0000

    Adapt to newer gdb

--- gdbdis.fs
+++ gdbdis.fs
@@ -7,7 +7,7 @@ libc getpid (ptr) getpid
 : seecode ( start end -- )
    s" /tmp/gdbcmds" w/o open throw script 2!
    s" attach " >s  getpid 0 (d.) >s  newline >s
-   s" disassemble " >s  swap n>s >s  s"  " >s  n>s >s  newline >s
+   s" disassemble " >s  swap n>s >s  s" ," >s  n>s >s  newline >s
    script 2@ close throw
    s" pipe://gdb --batch --command=/tmp/gdbcmds" r/o open throw
    here 4096 2over read drop here swap type close throw ;
